a2036666d6cc103a17f5611f82f4cccce564cca4
Kogut <Qkii@mail.ru>
2022-12-07 00:24:44 +0300

    1

---
+++ 1.8/src/main/java/org/example/app/services/BookRepository.java
@@ -0,0 +1,49 @@
+package org.example.app.services;
+
+import org.apache.log4j.Logger;
+import org.example.web.dto.Book;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Repository
+public class BookRepository implements ProjectRepository<Book> {
+
+    private final Logger logger = Logger.getLogger(BookRepository.class);
+    private final List<Book> repo = new ArrayList<>();
+
+    @Override
+    public List<Book> retreiveAll() {
+        return new ArrayList<>(repo);
+    }
+
+    @Override
+    public void store(Book book) {
+        book.setId(book.hashCode());
+        logger.info("store new book: " + book);
+        repo.add(book);
+    }
+
+    @Override
+    public boolean removeItemById(Integer bookIdToRemove) {
+        for (Book book : retreiveAll()) {
+            if (book.getId().equals(bookIdToRemove)) {
+                logger.info("remove book completed: " + book);
+                return repo.remove(book);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeByRegex(String reg) {
+        for (Book book : retreiveAll()) {
+            if (book.getAuthor().equals(reg) || book.getTitle().equals(reg) || Objects.equals(book.getSize(), Integer.valueOf(reg))) {
+                repo.remove(book);
+            }
+        }
+        return false;
+    }
+}
